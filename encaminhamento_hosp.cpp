5a9d428eb1ef42e28acb737ab8fbefdf03efc6b8
Joao Pedro Martins <jotapmartins07@gmail.com>
2025-11-17 17:14:37 -0300

    Algoritimo

--- encaminhamento_hosp.cpp
+++ encaminhamento_hosp.cpp
@@ -6,22 +6,22 @@ int main(){
         printf("Digite a temperatura corporal do paciente: ");
         scanf("%f", &tempCorp);
         if(tempCorp < 35){
-            printf("Condição: Hipotermia | ENCAMINHAR PARA SALA VERMELHA - URGÊNCIA");
+            printf("Condição: Hipotermia | ENCAMINHAR PARA SALA VERMELHA - URGÊNCIA \n");
         }
         else{
             if(tempCorp <= 37){
-                printf("Condição: Normal | ENCAMINHAR PARA SALA VERDE - ESPERA");
+                printf("Condição: Normal | ENCAMINHAR PARA SALA VERDE - ESPERA \n");
             }
             else{
                 if(tempCorp <= 37.7){
-                    printf("Condição: Estado Febril | ENCAMINHAR PARA SALA AZUL - OBSERVAçÃO");
+                    printf("Condição: Estado Febril | ENCAMINHAR PARA SALA AZUL - OBSERVAÇÃO \n");
                 }
                 else{
                     if(tempCorp <= 38.9){
-                        printf("Condição: Febre | ENCAMINHAR PARA SALA AMARELA - URGÊNCIA");
+                        printf("Condição: Febre | ENCAMINHAR PARA SALA AMARELA - URGÊNCIA \n");
                     }
                     else{
-                        printf("Condição: Febre Alta | ENCAMINHAR PARA SALA BRANCA - ISOLAMENTO");
+                        printf("Condição: Febre Alta | ENCAMINHAR PARA SALA BRANCA - ISOLAMENTO \n");
                     }
                 }
             }
